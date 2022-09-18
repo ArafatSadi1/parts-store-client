a9ce7c120ece261c8d6e2c4974232c1374372628
ArafatSadi1 <arafatsadi1@gmail.com>
2022-09-18 19:41:26 +0600

    card design update

--- src/components/pages/Home/Products/Product.js
+++ src/components/pages/Home/Products/Product.js
@@ -5,33 +5,34 @@ const Product = ({ product }) => {
   const { _id, price, about, picture, name, available, minOrder } = product;
 
   return (
-    <div className="card bg-base-100 shadow-xl">
+    <div className="card bg-base-100 shadow-xl border border-primary">
       <figure>
         <img className="w-full" src={picture} alt="car parts" />
       </figure>
-      <div className="card-body text-gray-700">
-        <h2 className="card-title text-2xl">
-          <span className="text-neutral">Parts:</span> {name}
-        </h2>
-        <p className="text-gray-500">
-          <span className="font-semibold text-gray-700">About Parts: </span>
-          {about}
-        </p>
-        <div>
-          <p className="font-semibold">
-            Price: ${price}/<span className="text-slate-500">piece</span>
-          </p>
-          <p className="font-semibold py-0">
-            Available: {available}
-            <span className="text-slate-500"> piece</span>
-          </p>
-          <p className="font-semibold">
-            Min Order Qty: {minOrder}
-            <span className="text-slate-500"> piece</span>
-          </p>
+      <div className="text-gray-700">
+        <div className="p-2">
+          <h2 className="card-title text-2xl">
+            <span className="text-neutral">Parts:</span> {name}
+          </h2>
+          <div className="my-3">
+            <p className="font-semibold">
+              Price: ${price}/<span className="text-slate-500 text-sm">piece</span>
+            </p>
+            <p className="font-semibold py-0">
+              Available: {available}
+              <span className="text-slate-500 text-sm"> piece</span>
+            </p>
+            <p className="font-semibold">
+              Min Order Qty: {minOrder}
+              <span className="text-slate-500 text-sm"> piece</span>
+            </p>
+          </div>
         </div>
-        <div className="card-actions justify-end">
-          <Link to={`/purchase-product/${_id}`} className="btn btn-primary">
+        <div>
+          <Link
+            to={`/purchase-product/${_id}`}
+            className="btn btn-primary w-full rounded-none"
+          >
             Place Order
           </Link>
         </div>
